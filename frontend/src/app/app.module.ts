b372a9737b5c22a1e0ecc628fd12ad5048080842
chisinus <jun.xia@fevo.com>
2026-05-23 10:40:09 -0400

    upgrade to 18

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { routes } from "./app.routes";
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(routes),
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(routes)], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
